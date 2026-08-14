b5f80eca3a70bb743310056bcdceb845d4114e0c
Jibin <Jibinchelakattu555@gmail.com>
2026-08-14 13:02:11 +0530

    Change whatsapp number

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
         <main className="min-h-screen">{children}</main>
         <Footer />
         {/* Replace with your actual WhatsApp number */}
-        <WhatsAppFloat phoneNumber="917592971028" />
+        <WhatsAppFloat phoneNumber="918089641028" />
       </body>
     </html>
   );
